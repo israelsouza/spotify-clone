5b476ad84e4bef22b9e025c2edeb70236046e201
Israel de Souza <israellimas@hotmail.com>
2025-02-24 20:37:03 -0300

    feat: add props, db; using db
    - usando valores do componente pai por props; - enviando valores pro componente filho por props; - construindo uma lógica para consumir a db no componente filho

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import SingleItem from "./SingleItem";
 import style from "./styles/Main.module.css";
+import { artistArray } from "../assets/database/artists";
 
-const ItemList = () => {
+const ItemList = ({ title, items }) => {
   return (
     <div className={style.itemList}>
       <div className={style.itemList__header}>
-        <h2>Artistas populares</h2>
+        <h2>{title} populares</h2>
         <a className={style.main__link} href="/">
           Mostrar tudo
         </a>
       </div>
 
       <div className={style.itemList__container}>
-        <SingleItem />
-
-        <SingleItem />
-
-        <SingleItem />
-
-        <SingleItem />
+        {artistArray
+          .filter((currValue, index) => index < items)
+          .map((currentValue, index) => {
+            return (
+              <SingleItem
+                key={`${title}-${index}`}
+                name={currentValue.name}
+                image={currentValue.image}
+              />
+            );
+          })}
       </div>
     </div>
   );
