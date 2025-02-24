93e3c5c47fcf162259976c678286d4ca6c2a3ec3
Israel de Souza <israellimas@hotmail.com>
2025-02-24 20:33:22 -0300

    feat: using props values

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import style from "./styles/Main.module.css";
 import { FaPlayCircle } from "react-icons/fa";
 
-const SingleItem = () => {
+const SingleItem = ({ name, image }) => {
+    console.log(image)
   return (
     <div className={style.singleItem}>
       <div className={style.itemList__div_image_button}>
         <div className={style.itemList__div_image}>
           <img
             className={style.singleItem__image}
-            src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
-            alt="Imagem do artista X"
+            src={image}
+            alt={`Imagem do artista ${name}`}
           />
         </div>
         <FaPlayCircle className={style.singleItem__icon} />
       </div>
 
       <div className={style.singleItem__text}>
-        <p className={style.singleItem__title}>Henrique & Juliano</p>
+        <p className={style.singleItem__title}>{name}</p>
         <p className={style.singleItem__type}>Artista</p>
       </div>
     </div>
