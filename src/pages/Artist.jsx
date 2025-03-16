b9238f51793006301a5aed8078f588bb91af6121
Israel de Souza <israellimas@hotmail.com>
2025-03-16 12:49:40 -0300

    feat: add randomId para tocar musica aleatoria do artista

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import "../assets/css/artist.css";
+import React, { use } from "react";
+import { FaPlayCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import SongList from "../components/SongList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
+import { useParams } from "react-router-dom";
 
 const Artist = () => {
-  return (
-    <div>Artist</div>
-  )
-}
+  const { id } = useParams();
 
-export default Artist
+  const { name, banner } = artistArray.filter(
+    (currentArtistObj) => currentArtistObj.id === Number(id)
+  )[0];
+
+  const songsArrayFromArtist = songsArray.filter(
+    (currentSongObj) => currentSongObj.artist === name
+  );
+
+  const randomIdSongFromArtist = Math.floor(
+    Math.random() * (songsArrayFromArtist.length - 1)
+  );
+
+  const randomIndex = songsArrayFromArtist[randomIdSongFromArtist].id;
+
+  return (
+    <div className="artist">
+      <div
+        className="artist__header"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, var(--_shade), var(--_shade)),url(${banner})`,
+        }}
+      >
+        <h2 className="artist__title">{name} </h2>
+      </div>
+
+      <div className="artist__body">
+        <h2>Populares</h2>
+
+        <SongList listSongsFromArtist={songsArrayFromArtist} />
+      </div>
+
+      <Link className="single-item__icon--fixed" to={`/song/${randomIndex}`}>
+        <FaPlayCircle />
+      </Link>
+    </div>
+  );
+};
+
+export default Artist;
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import Player from "../components/Player";
+import { Link, useParams } from "react-router-dom";
+import { songsArray } from "../assets/database/songs";
+import { artistArray } from "../assets/database/artists";
 
 const Song = () => {
-  return (
-    <div>Song</div>
-  )
-}
+  const { id } = useParams();
 
-export default Song
+  const { name, duration, image, artist } = songsArray.filter(
+    (currSongObj) => currSongObj.id === Number(id)
+  )[0];
+
+  const artistObj = artistArray.filter(
+    (currArtistObj) => currArtistObj.name === artist
+  )[0];
+
+  const songsArrayFromArtist = songsArray.filter(
+    (currValue, index) => currValue.artist === artist
+  );
+
+  const randomIndex = Math.floor(
+    Math.random() * (songsArrayFromArtist.length - 1)
+  );
+
+  const randomIndexTwo = Math.floor(
+    Math.random() * (songsArrayFromArtist.length - 1)
+  );
+
+  const randomIdFromArtist = songsArrayFromArtist[randomIndex].id;
+  const randomIdFromArtistTwo = songsArrayFromArtist[randomIndexTwo].id;
+
+  return (
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img src={image} alt={`Imagem da musica ${name}`} />
+        </div>
+      </div>
+
+      <div className="song__bar">
+        <Link to={`/artist/${artistObj.id}`} className="song__artist-image">
+          <img
+            src={artistObj.image}
+            alt={`Imagem do artista ${artist}`}
+            width={"75px"}
+            height={"75px"}
+          />
+        </Link>
+
+        <Player
+          duration={duration}
+          randomIdFromArtist={randomIdFromArtist}
+          randomIdFromArtistTwo={randomIdFromArtistTwo}
+        />
+
+        <div className="song__info">
+          <p className="song__name">{name}</p>
+          <p className="song__artist">{artist}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Song;
